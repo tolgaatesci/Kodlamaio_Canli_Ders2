fb91b00b884fc7c7b507a0f86163b724df06f5e0
Tolga Ateşci <x-man_3737@hotmail.com>
2021-01-16 23:50:10 +0300

    as

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -7,7 +7,7 @@ namespace DegerVeReferansTipler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+                
             int sayi1 = 10;
             int sayi2 = 30;
             sayi1 = sayi2;
