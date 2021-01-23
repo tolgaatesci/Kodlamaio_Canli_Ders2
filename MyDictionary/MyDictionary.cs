605909594c5c41f910a1f78985558db83baa47cf
Tolga Ateşci <x-man_3737@hotmail.com>
2021-01-23 17:55:05 +0300

    23.01.2021

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, T2>
+    {
+        T[] items;
+        T2[] items2;
+
+        public MyDictionary()
+        {
+            items = new T[0];
+            items2 = new T2[0];
+        }
+
+        public void Add(T item, T2 item2) 
+        {
+            
+            T[] tempArray = items;  
+            items = new T[items.Length + 1]; 
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+            T2[] tempArray2 = items2;
+            items2 = new T2[items2.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                items2[i] = tempArray2[i];
+            }
+
+            items2[items.Length - 1] = item2;
+        }
+
+        public void Goster()
+        {
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i].ToString() + " " + items2[i]);
+
+            }
+
+        }
+
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenci = new Dictionary<int, string>();
+            ogrenci.Add(123, "Tolga");
+            ogrenci.Add(456, "Ahmet");
+            ogrenci.Add(789, "Mehmet");
+
+            foreach (var ogrenciler in ogrenci)
+            {
+                Console.WriteLine(ogrenciler);
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            Console.WriteLine(ogrenci.Count);
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            MyDictionary<int, string> ogrenci2 = new MyDictionary<int, string>();
+            ogrenci2.Add(123, "Tolga");
+            ogrenci2.Add(456, "Ahmet");
+            ogrenci2.Add(789, "Mehmet");
+
+            ogrenci2.Goster();
+
+            
+        }
+    }
+}
